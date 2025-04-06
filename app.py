54c0f855917dbc9223b8b1d8bd700285c8ce2083
varunkm11 <varunkumarsingh818@gmail.com>
2025-04-07 01:06:19 +0530

    Day -2 Medical Diagnosis App

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ import pandas as pd
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+
+#Load Model and Vectorizer
+model = joblib.load("model/diagnosis_model.pkl")
+vectorizer = joblib.load("model/tfidf_vectorizer.pkl")
+
+st.title("🩺 AI Medical Diagnosis")
+st.write("Enter your symptoms and get a possible disease prediction.(Disclaimer: Do not trust 100%)")
