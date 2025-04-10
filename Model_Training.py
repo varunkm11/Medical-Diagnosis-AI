9f1ed2e8a5d46630e26d3032aa9da4acaeb8544e
varunkm11 <varunkumarsingh818@gmail.com>
2025-04-10 22:27:39 +0530

    Day 5 Medical diagnosis App (2)

--- Model_Training.py
+++ Model_Training.py
@@ -3,3 +3,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import joblib
 import os
+#Load and preprocess
+df = pd.read_csv("data/medical_data.csv")
+
+x = df["symptoms"]
+y = df["disease"]
+
+vectorizer = TfidfVectorizer()
+x_vec = vectorizer
